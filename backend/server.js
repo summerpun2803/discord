8cc07a455b554093699a30da6b3a1dd0f4e79842
summerpun2803 <mastersam2003@gmail.com>
2024-03-30 20:46:52 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ socketServer.registerSocketServer(server)
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
-        server.listen(process.env.PORT, () => {
+        server.listen(process.env.PORT || 5000 , () => {
             console.log("listening to the port")
         })
     })
